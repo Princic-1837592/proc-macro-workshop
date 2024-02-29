7e9e7791032414693ede2499124250a4e80240ed
Princic-1837592 <andreapri.99@gmail.com>
2024-02-29 01:16:13 +0100

    implemented up to test 6
    with new version of test 6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,12 @@
-use std::iter::Map;
-
+use deluxe::{parse_attributes, ParseAttributes};
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, quote_spanned};
 use syn::{
-    punctuated::Iter, spanned::Spanned, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed,
+    spanned::Spanned, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, GenericArgument,
+    PathSegment, Type, TypePath,
 };
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let derive_input: DeriveInput = syn::parse(input).unwrap();
     let ident = &derive_input.ident;
@@ -82,46 +82,141 @@ fn defaults(data: &Data) -> TokenStream {
     }
 }
 
-fn unwraps(fields: &FieldsNamed) -> Map<Iter<Field>, fn(&Field) -> TokenStream> {
-    fields.named.iter().map(|f| {
-        let name = &f.ident;
-        quote_spanned! {
-            f.span() => #name: self.#name.clone().unwrap()
-        }
-    })
+#[derive(ParseAttributes, Debug)]
+#[deluxe(attributes(builder))]
+struct BuilderAttributes {
+    optional: Option<bool>,
 }
 
-fn none_checks(fields: &FieldsNamed) -> Map<Iter<Field>, fn(&Field) -> TokenStream> {
-    fields.named.iter().map(|f| {
-        let name = f.clone().ident.unwrap();
-        let err = format!("{} was not set", name);
-        quote_spanned!(
-            f.span() => if self.#name.is_none() {
-                return std::result::Result::Err(#err.to_owned().into());
-            }
+fn is_optional(field: &Field) -> bool {
+    field.attrs.iter().any(|attr| {
+        let default: Result<BuilderAttributes, _> = parse_attributes(attr);
+        matches!(
+            default,
+            Ok(BuilderAttributes {
+                optional: Some(true),
+                ..
+            })
         )
     })
 }
 
-fn option_wrapped(fields: &FieldsNamed) -> Map<Iter<Field>, fn(&Field) -> TokenStream> {
-    fields.named.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
-        quote_spanned! {
-            f.span() => #name: std::option::Option<#ty>
+fn get_inner(field: &Field) -> Option<TokenStream> {
+    if is_optional(field) {
+        match &field.ty {
+            Type::Path(TypePath {
+                path: syn::Path { segments, .. },
+                ..
+            }) => {
+                if segments.len() != 1 {
+                    return None;
+                }
+                let segment = segments.iter().next().unwrap();
+                match segment {
+                    PathSegment {
+                        arguments:
+                            syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                                args,
+                                ..
+                            }),
+                        ..
+                    } => {
+                        if args.len() != 1 {
+                            return None;
+                        }
+                        let arg = args.iter().next().unwrap();
+                        match arg {
+                            GenericArgument::Type(t) => Some(quote!(#t)),
+                            _ => None,
+                        }
+                    }
+                    _ => None,
+                }
+            }
+            _ => None,
         }
-    })
+    } else {
+        None
+    }
 }
 
-fn setters(fields: &FieldsNamed) -> Map<Iter<Field>, fn(&Field) -> TokenStream> {
-    fields.named.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
-        quote_spanned! {
-            f.span() => fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = std::option::Option::Some(#name);
-                self
+fn unwraps(fields: &FieldsNamed) -> Vec<TokenStream> {
+    fields
+        .named
+        .iter()
+        .map(|f| {
+            let name = &f.ident;
+            if is_optional(f) {
+                quote_spanned! {
+                    f.span() => #name: self.#name.clone()
+                }
+            } else {
+                quote_spanned! {
+                    f.span() => #name: self.#name.clone().unwrap()
+                }
             }
-        }
-    })
+        })
+        .collect()
+}
+
+fn none_checks(fields: &FieldsNamed) -> Vec<TokenStream> {
+    fields
+        .named
+        .iter()
+        .filter_map(|f| {
+            if is_optional(f) {
+                None
+            } else {
+                let name = f.clone().ident.unwrap();
+                let err = format!("{} was not set", name);
+                Some(quote_spanned!(
+                    f.span() => if self.#name.is_none() {
+                        return std::result::Result::Err(#err.to_owned().into());
+                    }
+                ))
+            }
+        })
+        .collect()
+}
+
+fn option_wrapped(fields: &FieldsNamed) -> Vec<TokenStream> {
+    fields
+        .named
+        .iter()
+        .map(|f| {
+            let name = &f.ident;
+            let ty = &f.ty;
+            if is_optional(f) {
+                quote_spanned! {
+                    f.span() => #name: #ty
+                }
+            } else {
+                quote_spanned! {
+                    f.span() => #name: std::option::Option<#ty>
+                }
+            }
+        })
+        .collect()
+}
+
+fn setters(fields: &FieldsNamed) -> Vec<TokenStream> {
+    fields
+        .named
+        .iter()
+        .map(|f| {
+            let name = &f.ident;
+            let ty = &f.ty;
+            let actual_ty = if let Some(inner) = get_inner(f) {
+                quote!(#inner)
+            } else {
+                quote!(#ty)
+            };
+            quote_spanned! {
+                f.span() => fn #name(&mut self, #name: #actual_ty) -> &mut Self {
+                    self.#name = std::option::Option::Some(#name);
+                    self
+                }
+            }
+        })
+        .collect()
 }
