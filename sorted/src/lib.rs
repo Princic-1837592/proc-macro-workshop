001afc48c907870b13fe846cb95ecb9c3762bbdc
Princic-1837592 <andreapri.99@gmail.com>
2024-03-03 00:50:55 +0100

    implemented up to test 4

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,5 +1,6 @@
+use proc_macro2::{Span, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Item};
+use syn::{parse_macro_input, spanned::Spanned, Error, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(
@@ -7,6 +8,31 @@ pub fn sorted(
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let item = parse_macro_input!(input as Item);
+    match delegate_sorted(item) {
+        Ok(result) => quote!(#result),
+        Err(error) => error.to_compile_error(),
+    }
+    .into()
+}
 
-    quote!(#item).into()
+fn delegate_sorted(input: Item) -> syn::Result<TokenStream> {
+    if let Item::Enum(item_enum) = input {
+        let names: Vec<_> = item_enum.variants.iter().map(|v| &v.ident).collect();
+        let mut sorted = names.clone();
+        sorted.sort_by_key(|v| v.to_string());
+        for (original, sorted) in names.iter().zip(&sorted) {
+            if original != sorted {
+                return Err(Error::new(
+                    sorted.span(),
+                    format!("{} should sort before {}", sorted, original),
+                ));
+            }
+        }
+        Ok(quote!(#item_enum))
+    } else {
+        Err(Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        ))
+    }
 }
