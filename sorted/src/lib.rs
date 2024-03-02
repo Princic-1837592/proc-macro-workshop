12ddaac5f41c9a808f2d613cf547bea0b0f6e02a
Princic-1837592 <andreapri.99@gmail.com>
2024-03-02 16:13:30 +0100

    setup sorted and test 1

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,12 @@
-use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let item = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    quote!(#item).into()
 }
