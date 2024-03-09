0ea93d4566b6011832d4b00e64dcf0b3bd1e4335
Princic-1837592 <andreapri.99@gmail.com>
2024-03-09 17:21:20 +0100

    implemented up to test 1

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 
 #[proc_macro_attribute]
 pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -6,4 +7,20 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = input;
 
     unimplemented!()
+}
+
+#[proc_macro]
+pub fn generate_bs(_: TokenStream) -> TokenStream {
+    let range = 1..=64_usize;
+    let names = range.clone().map(|i| format_ident!("B{}", i));
+    quote!(
+        #(
+            pub enum #names {}
+
+            impl Specifier for #names {
+                const BITS: usize = #range;
+            }
+        )*
+    )
+    .into()
 }
--- bitfield/src/lib.rs
+++ bitfield/src/lib.rs
@@ -10,6 +10,10 @@
 //
 // From the perspective of a user of this crate, they get all the necessary APIs
 // (macro, trait, struct) through the one bitfield crate.
-pub use bitfield_impl::bitfield;
+pub use bitfield_impl::{bitfield, generate_bs};
 
-// TODO other things
+pub trait Specifier {
+    const BITS: usize;
+}
+
+generate_bs!();
