6bcf7f082933286bff4f4010e5bc8c9eeb037bf2
Princic-1837592 <andreapri.99@gmail.com>
2024-03-09 17:40:43 +0100

    implemented up to test 2

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,12 +1,35 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
-pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn bitfield(_: TokenStream, input: TokenStream) -> TokenStream {
+    expand(parse_macro_input!(input as Item)).into()
+}
 
-    unimplemented!()
+fn expand(item: Item) -> proc_macro2::TokenStream {
+    match item {
+        Item::Struct(syn::ItemStruct {
+            attrs,
+            vis,
+            ident,
+            generics,
+            fields: syn::Fields::Named(fields),
+            ..
+        }) => {
+            let (impl_generics, _ty_generics, where_clause) = generics.split_for_impl();
+            let field_types = fields.named.clone().into_iter().map(|f| f.ty);
+            let sums = quote!((#(<#field_types as ::bitfield::Specifier>::BITS)+*));
+            quote!(
+                #(#attrs)*
+                #[repr(C)]
+                #vis struct #ident #impl_generics #where_clause {
+                    data: [u8; #sums / 8 + if #sums % 8 == 0 { 0 } else { 1 }],
+                }
+            )
+        }
+        _ => unimplemented!(),
+    }
 }
 
 #[proc_macro]
