85933022643aa0cde9e5d19abebb38458876a476
Princic-1837592 <andreapri.99@gmail.com>
2024-03-17 12:59:35 +0100

    implemented up to test 4

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,6 +1,8 @@
-use proc_macro2::TokenStream;
-use quote::quote;
-use syn::Error;
+use std::iter;
+
+use proc_macro2::{Group, Ident, Literal, TokenStream, TokenTree};
+use quote::format_ident;
+use syn::{spanned::Spanned, Error};
 
 use crate::parser::{
     maybe_next_punct, next_braces, next_end, next_ident, next_keyword, next_punct, next_value,
@@ -32,5 +34,48 @@ fn internal(input: TokenStream) -> syn::Result<TokenStream> {
             "Empty range",
         ));
     }
-    Ok(quote!())
+    let mut result = TokenStream::new();
+    for val in start..=if inclusive { end } else { end - 1 } {
+        result.extend(replace_value(&var, val, body.clone()));
+    }
+    Ok(result)
+}
+
+fn replace_value(var: &Ident, val: u64, body: TokenStream) -> TokenStream {
+    let mut tokens = Vec::from_iter(body);
+    let mut i = 0;
+    while i < tokens.len() {
+        match &tokens[i] {
+            TokenTree::Ident(ident) if ident == var => {
+                let mut literal = Literal::u64_unsuffixed(val);
+                literal.set_span(ident.span());
+                tokens[i] = TokenTree::Literal(literal);
+                i += 1;
+                continue;
+            }
+            _ => {}
+        }
+        if i + 2 < tokens.len() {
+            match &tokens[i..=i + 2] {
+                [TokenTree::Ident(prefix), TokenTree::Punct(punct), TokenTree::Ident(ident)]
+                    if ident == var && punct.as_char() == '~' =>
+                {
+                    let mut ident = format_ident!("{}{}", prefix, val);
+                    ident.set_span(prefix.span());
+                    tokens.splice(i..i + 3, iter::once(TokenTree::Ident(ident)));
+                    i += 3;
+                    continue;
+                }
+                _ => {}
+            }
+        }
+        if let TokenTree::Group(group) = &mut tokens[i] {
+            let original_span = group.span();
+            let content = replace_value(var, val, group.stream());
+            *group = Group::new(group.delimiter(), content);
+            group.set_span(original_span);
+        }
+        i += 1;
+    }
+    TokenStream::from_iter(tokens)
 }
